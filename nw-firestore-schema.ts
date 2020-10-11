fc517a3d1d6a17fa93f6a69235b95b098d60de69
jackyzha0 <j.zhao2k19@gmail.com>
2020-10-11 16:27:32 -0700

    update livesite dayof events schema

--- nw-firestore-schema.ts
+++ nw-firestore-schema.ts
@@ -50,7 +50,8 @@ interface Quicklink {
 
 interface DayOfEvent {
   name: "name";
-  type: "meal" | "workshop" | "hacking";
+  type: "notices" | "main" | "workshops" | "minievents";
+  delayed: boolean;
   startTime: "July 8, 2020 at 12:00:00PM UTC-7";
   endTime: "July 8, 2020 at 2:00:00PM UTC-7";
 }
